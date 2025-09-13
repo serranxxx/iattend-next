22165f027c50da138cb06005886133664f35d338
albserranog <105310020+serranxxx@users.noreply.github.com>
2025-09-13 17:09:48 -0600

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { AppProvider } from "@/context/AppProvider";
 import "@/styles/globals.css";
 import { AntdProvider } from "@/context/AntdProvider";
-import Script from "next/script";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "I attend",
   description: "Diseña, comparte, celebra.",
+  keywords: ["invitaciones digitales", "bodas", "fiestas", "i attend"],
 };
 
 export default function RootLayout({
@@ -27,16 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anton+SC&family=Kaisei+Opti&family=Lilita+One&family=Outfit:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Libre+Franklin:ital,wght@0,100..900;1,100..900&family=Signika:wght@300..700&family=Comfortaa:wght@300..700&family=DM+Serif+Display:ital@0;1&family=Dancing+Script:wght@400..700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Platypi:ital,wght@0,300..800;1,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Quicksand:wght@300..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Sedan:ital@0;1&family=Work+Sans:ital,wght@0,100..900;1,100..900&family=Cedarville+Cursive&family=Edu+NSW+ACT+Cursive:wght@400..700&family=Fredoka:wght@300..700&family=Tangerine:wght@400;700&family=WindSong:wght@400;500&family=Monsieur+La+Doulaise&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
         <AppProvider>
-          <AntdProvider>
-            {children}
-            <Script
-              id="pinterest-sdk"
-              src="https://assets.pinterest.com/js/pinit.js"
-              strategy="afterInteractive"
-            />
-          </AntdProvider>
+          <AntdProvider>{children}</AntdProvider>
         </AppProvider>
       </body>
     </html>
